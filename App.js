0287966fa804e8c1482ebbdb87e996596d1ff742
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-18 22:48:27 +0300

    react native first project files created

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ogwegwegwegwegweg</Text>
+      <Text style={styles.setFontSizeFour}>Hello this is hello world really</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  setFontSizeFour: {
+    fontSize: 30,
+  },
+});
