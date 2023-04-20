f2fa021c74868d3e3534831f2b715bdbf0c2bec1
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-04-20 12:00:39 +0300

    text input fields added with props

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState } from "react";
 
 export default function App() {
 
   const[name, setName] = useState("elefantenberg");
+  let[yourName, setYourName] = useState("");
   const[client, setClient] = useState({
     city: "diyarbakir", 
     age: 40,
@@ -16,16 +17,28 @@ export default function App() {
     } else {
       setName("abdulhakim");  
     }
-
   }
 
   return (
     <View style={styles.container}>
       <Text>My Name is: {name} </Text>
+      <Text>Your Name is: {yourName}</Text>
       <Text>You are from: {client.city} and your age is: {client.age} </Text>
       <Text style={styles.setFontSizeFour}>Hello this is hello world really</Text>
       <View style={styles.buttonContainer}>
         <Button title='update name' onPress={changeName} />
+      </View>
+      <View>
+        <Text>Enter Name: </Text> 
+        <TextInput 
+          multiline //when you press enter it will go one line down
+          numberOfLines={3}
+          maxLength={40} 
+          keyboardType='numeric' //the keyboard that will pop up will be numeric
+          style={styles.inputField} 
+          onChangeText={(val)=> setYourName(val)}
+          placeholder='your name....'
+         />
       </View>
     </View>
   );
@@ -45,5 +58,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 50,
-  }
+  },
+  inputField: {
+    backgroundColor: "gray",
+    fontSize: 25,
+    borderRadius: 7,
+    fontFamily: "Verdana",
+    padding:9,
+  },
 });
